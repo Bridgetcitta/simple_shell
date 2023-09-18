3f71f7f98efe6d7c5711b4833d96cd20ae98b1cc
tseleng <dikgalembali@gmail.com>
2023-09-18 10:59:52 -0700

    simple_shell

--- shell.h
+++ shell.h
@@ -12,5 +12,8 @@ void _prompt(void);
 void _print(const char *format);
 void read_input(char *format, size_t size);
 void _execute(const char *format);
+int main(void);
+int main(int ac, char **av);
+int main(int ac, char **av, char **env);
 
 #endif
