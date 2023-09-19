a188aece267845364f1a984975df9159172a1597
tseleng <dikgalembali@gmail.com>
2023-09-19 06:26:26 -0700

    number

--- shell.h
+++ shell.h
@@ -12,8 +12,16 @@ void _prompt(void);
 void _print(const char *format);
 void read_input(char *format, size_t size);
 void _execute(const char *format);
-int main(void);
-int main(int ac, char **av);
-int main(int ac, char **av, char **env);
+int _atoi(char *s);
+int _isalpha(int c);
+int interactive(info_t *info);
+int _myhelp(info_t *info);
+int _mycd(info_t *info);
+int _myexit(info_t *info);
+int _myalias(info_t *info);
+int print_alias(list_t *node);
+int set_alias(info_t *info, char *str);
+int unset_alias(info_t *info, char *str);
+int _myhistory(info_t *info);
 
 #endif
