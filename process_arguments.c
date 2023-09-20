8a6dfcbcabfe0afd574453d9adbc9cf6591ce8a1
Bridgetcitta <oforibridget20@gmail.com>
2023-09-20 13:14:02 -0700

    Updated files: process_arguments.c seth_path.c

--- process_arguments.c
+++ process_arguments.c
@@ -1,7 +1,5 @@
 #include "shell.h"
 #include <stdio.h>
-#define MAX_ARGUMENTS 10
-
 /**
  * process_arguments - handles arguments
  * @input: parameter memeber
@@ -9,43 +7,44 @@
  */
 void process_arguments(const char *input)
 {
-	/* Create a non-const copy of the input */
-	char *input_copy;
+	char *input_copy = NULL;
 	char *command = NULL;
 	char *arguments[MAX_ARGUMENTS];
 	int arg_count = 0;
+	int i;
 	char *token = strtok(input_copy, " ");
 
-	strcpy(input_copy, input);
-
-	input_copy = malloc(strlen(input) + 1);
-	if (input_copy == NULL)
+	if (input != NULL)
 	{
+		input_copy = malloc(strlen(input) + 1);
+		if (input_copy == NULL)
+		{
 		perror("memoey allocation failed\n");
 		exit(EXIT_FAILURE);
+		}
+		strcpy(input_copy, input);
 	}
-
 	token = strtok(input_copy, " ");
-
 	while (token != NULL)
 	{
 		if (arg_count == 0)
 		{
-		/* The first token is the command. */
 		command = token;
 		}
 		else if (arg_count < MAX_ARGUMENTS)
 		{
-		/* Store subsequent tokens as arguments.*/
 		arguments[arg_count - 1] = token;
 		}
 
 		token = strtok(NULL, " ");
 		arg_count++;
 	}
-
-	/* Null-terminate the arguments array. */
 	arguments[arg_count - 1] = NULL;
-
+	_print("command: %s\n", command);
+	_print("arguments:\n");
+	for (i = 0; i < arg_count - 1; i++)
+	{
+		_print("%d. %s\n", i + 1, arguments[i]);
+	}
 	free(input_copy);
 }
