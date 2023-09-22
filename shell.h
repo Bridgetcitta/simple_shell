fcd1581e020c01569e64250d8f9b2984ea6815d4
Bridgetcitta <oforibridget20@gmail.com>
2023-09-21 19:52:22 -0700

    new

--- shell.h
+++ shell.h
@@ -13,24 +13,26 @@
 #define MAX_INPUT_SIZE 256
 #define MAX_ARGUMENTS 128
 
-/* Structure to store aliases */
-struct Alias 
+/**
+ * struct Alias - Structure to store aliases
+ * @name: name
+ * @value: value
+ */
+typedef struct Alias
 {
-    char *name;
-    char *value;
+	char *name;
+	char *value;
 };
-
 /* Global array to store aliases */
 struct Alias aliases[MAX_ALIASES];
-int aliasCount;
-void printAliases();
+void printAliases(void);
 int findAlias(char *name);
 int extractAlias(char *arg, char **name, char **value);
 void processAlias(char *args[]);
 void _print(const char *format, ...);
 void execute_command(char *command, char *arguments[]);
 int check_command_existence(char *command);
-void shell_exit();
+void shell_exit(void);
 ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream);
 void shell_exit(int status);
 int shell_setenv(char *name, char *value, int overwrite);
@@ -39,10 +41,4 @@ int _cd(const char *path);
 int process_cd(char *argv[]);
 int process_exit(int argc, char *argv[]);
 int custom_atoi(const char *str);
-
-
-
-
-
 #endif
-
